fdcccf0fe28e55c6c07d851797329e7876a799e2
tonyrud <tonrudny@gmail.com>
2017-08-08 19:14:55 -0400

    add jquery form

--- public/js/index.js
+++ public/js/index.js
@@ -9,6 +9,20 @@ socket.on('disconnect', function() {
   console.log('Disconnected from server')
 })
 
-socket.on('newMessage', function (msg) {
-  console.log('New Message', msg)
+socket.on('newMessage', function (message) {
+  console.log('New Message', message)
+  var li = jQuery(`<li></li>`)
+  li.text(`${message.from}: ${message.text}`)
+
+  $('#messages').append(li);
 })
+
+jQuery('#message-form').on('submit', function (e) { 
+  e.preventDefault()
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function () {
+    
+  })
+ })
