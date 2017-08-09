575bcd7be4af26a0b598d5fd6ef795ad0fac3df1
tonyrud <tonrudny@gmail.com>
2017-08-08 22:34:13 -0400

    add styling

--- public/js/index.js
+++ public/js/index.js
@@ -26,13 +26,17 @@ socket.on('newLocationMessage', function(message) {
 
 jQuery('#message-form').on('submit', function(e) {
   e.preventDefault()
+
+  const messageTextbox = jQuery('[name=message]')
   socket.emit(
     'createMessage',
     {
       from: 'User',
-      text: jQuery('[name=message]').val()
+      text: messageTextbox.val()
     },
-    function() {}
+    function() {
+      messageTextbox.val('')
+    }
   )
 })
 
@@ -43,14 +47,18 @@ locationButton.on('click', function() {
     return alert('Geolocation not supported by you browser')
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location...')
+
   navigator.geolocation.getCurrentPosition(
     function(position) {
+      locationButton.removeAttr('disabled').text('Send location')
       socket.emit('createLocationMessage', {
         lat: position.coords.latitude,
         long: position.coords.longitude
       })
     },
     function() {
+      locationButton.removeAttr('disabled').text('Send location')
       alert('Unable to fetch location')
     }
   )
