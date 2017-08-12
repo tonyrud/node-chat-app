3760006d7948dd80b40bbbd04780ada905631c7c
tonyrud <tonrudny@gmail.com>
2017-08-12 08:08:53 -0400

    add rooms

--- server/server.js
+++ server/server.js
@@ -39,13 +39,20 @@ io.on('connection', socket => {
   })
 
   socket.on('createMessage', (message, callback) => {
-    console.log('createMessage', message)
-    io.emit('newMessage', generateMessage(message.from, message.text))
+    const user = users.getUser(socket.id)
+    if (user && isRealString(message.text)) {
+      io.to(user.room).emit('newMessage', generateMessage(user.name, message.text))
+    }
     callback()
   })
-
+  
   socket.on('createLocationMessage', (coords) => {
-    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.lat, coords.long))
+    const user = users.getUser(socket.id)
+
+    if (user) {
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.lat, coords.long))
+      
+    }
   })
 
   socket.on('disconnect', () => {
